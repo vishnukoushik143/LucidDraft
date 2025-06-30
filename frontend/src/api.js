c57401b19430b4f74bebc88fe7dda0df6d5b98f1
vishnukoushik143 <vishnukoushik143@gmail.com>
2025-06-30 08:09:34 +0530

    skadooshmonday1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const API_URL = "https://luciddraftbackend.onrender.com";
 // const API_URL = "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
